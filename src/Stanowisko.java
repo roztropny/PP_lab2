0c5f764b4ab72f4fa0cd5e4522890d618615adfe
Jędrzej Wyzgała <jedrzejwyzgala@gmail.com>
2018-10-09 10:57:11 +0200

    stanowisko

--- src/Stanowisko.java
+++ src/Stanowisko.java
@@ -1,3 +1,15 @@
-public enum Stanowisko {
+public class Stanowisko {
+    public enum nazwa {
+        ABC, DEF, GHI
+    }
 
+    public enum hierarchia {
+        DYREKTOR, KIEROWNIK
+    }
+
+    private final String opis;
+
+    public Stanowisko(String opis) {
+        this.opis = opis;
+    }
 }
